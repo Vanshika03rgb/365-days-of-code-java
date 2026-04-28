c5620f42f833ab7454594aaedfec052c8b2b21ff
Vanshika Sharma <vhka.sharma@gmail.com>
2026-04-29 01:11:30 +0530

    Create HelloWorld.java
    Day 1 (Basics)

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWord{
+  public static void main (String[] args){
+    System.out.println("Hello World!"); //using println helps in printing statements in new line, alternatively "System.out.print()" can be used to print statements in the same line.
+  }
+}
